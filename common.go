3c8128669152b36b5c3c13a6e38a5566adacd3dc
CausingBrick <1520593807@qq.com>
2020-10-08 17:54:05 +0800

    Remove queue

--- common.go
+++ common.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"container/list"
 	"log"
 )
 
@@ -10,19 +9,4 @@ func CheckErr(err error, info ...string) {
 	if err != nil {
 		log.Println(info, err)
 	}
-}
-
-// Queue implements
-type Queue struct {
-	list.List
-}
-
-// EnQueue EnQueues
-func (q *Queue) EnQueue(v interface{}) {
-	q.PushBack(v)
-}
-
-// DeQueue DeQueues
-func (q *Queue) DeQueue(pdfinfo *PdfInfo) interface{} {
-	return (q.Remove(q.Back()))
 }
