8828eca10d8367c530b9be1cdde00365b8090a67
CausingBrick <1520593807@qq.com>
2020-10-09 14:12:28 +0800

    Add info str

--- common.go
+++ common.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
+)
+
+// Define print string kind
+var (
+	InfoStr  = Green("INFO  ")
+	ErrorStr = Green("Error  ")
 )
 
 // CheckErr checks if err is NULL
@@ -9,4 +16,14 @@ func CheckErr(err error, info ...string) {
 	if err != nil {
 		log.Println(info, err)
 	}
+}
+
+// Red returns a red string
+func Red(message string) string {
+	return fmt.Sprintf("\x1b[31m%s\x1b[0m", message)
+}
+
+// Green returns a green string
+func Green(message string) string {
+	return fmt.Sprintf("\x1b[32m%s\x1b[0m", message)
 }
